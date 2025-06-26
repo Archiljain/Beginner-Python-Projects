0b8df1e7f46fc5d9966e859ee6daee17c0120f65
Archiljain <jainarchil55@gmail.com>
2025-06-26 11:20:33 +0530

    Add files via upload

---
+++ chatbot/README.md.txt
@@ -0,0 +1,19 @@
+# Simple ChatBot (Python Console Version)
+
+This is a basic interactive chatbot built with Python. It asks users about their name, age, interests, favorite food, movie, color, and hobbies, and responds accordingly with fun replies.
+
+## 🔧 Features
+- Takes user input from the console
+- Friendly and casual chatbot experience
+- Simple decision logic based on user preferences
+
+## 🛠️ Requirements
+
+This chatbot requires **Python 3.x**. No external libraries are used.
+
+## 📦 Installation
+
+```bash
+git clone https://github.com/yourusername/chatbot.git
+cd chatbot
+python main.py
---
+++ chatbot/main.py
@@ -0,0 +1,55 @@
+print("chat bot")
+name = input("what is you name?")
+print("The name of the user is", name)
+print("Hello", name, "! Nice to meet you")
+age = input("what is your age?")
+print("ok")
+
+doyoulikecooking = input("do you like cooking?(y/n)")
+
+if (doyoulikecooking == 'y' or doyoulikecooking == 'Y'):
+  print("ohh! me too")
+
+favoritedish = input("Which is your favorite dish ?")
+if (favoritedish == 'rajma chawal'):
+  print("mine too! great we both like same dish.")
+else:
+  print("That is also tasty ! but i like rajma chawal")
+
+favoritemovie = input("Which is your favorite  movie?")
+if (favoritemovie == 'iron man'):
+    print("mine too!!! We have so much in common!")
+else:
+    print("That is great movie! but i like iron man!.")
+    
+favoritecolour = input("which is your favorite colour?")
+if (favoritecolour == 'blue'):
+      print("ohh! great mine too.")
+else:
+      print("okay, but mine is blue!")
+ 
+Hobbies = input("what are your hobbies?")
+print("okay! great")
+
+  
+print("it was nice talking you ! bye.. ")
+if(doyoulikecooking=='n' or doyoulikecooking=='N'):
+  print("aww shucks! I just love to cook")
+  favoritemovie = input("Which is your favorite  movie?")
+  if(favoritemovie=='iron man'):
+    print("mine too!!! We have so much in common!")
+  else:
+    print("That is great movie! but i like iron man!.")
+  
+    favoritecolour = input("which is your favorite colour?")
+    if(favoritecolour=='blue'):
+      print("ohh! great mine too.")
+    else:
+      print("okay, but mine is blue!")
+      
+    print("it was nice talking you ! bye.. ")
+  
+  
+  
+
+    
