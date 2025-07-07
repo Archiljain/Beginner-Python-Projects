d42618781f842730ea9aeb946ac4ff19878efd17
Archil <jainarchil55@gmail.com>
2025-07-07 13:49:47 +0530

    wonderful code learning

--- New folder/main.py
+++ New folder/main.py
@@ -1,13 +1,18 @@
+import random
+import string
+def generate_random_string(length):
+   characters = string.ascii_lowercase + string.digits
+   return ''.join(random.choice(characters) for i in range(length))
+coding = input("1 for coding or 0 for decoding ")
 st =input("Enter message :")
 words = st.split(" ")
-coding = input("1 for coding or 0 for decoding ")
 coding = True if coding == "1" else False
 if(coding):
    nwords = []
    for word in words:
     if(len(word)>= 3):
-       r1 = "dsa"
-       r2 = "gfg"
+       r1 = generate_random_string(3)
+       r2 = generate_random_string(3)
        stnew = r1 + word[1:] + word[0] + r2
        nwords.append(stnew)
     else:
@@ -18,7 +23,7 @@ else:
  for word in words:
     if(len(word)>= 3):
        stnew = word[3:-3]
-       stnew = stnew[-1] + stnew[:-1]
+       stnew = stnew[-1:] + stnew[:-1]
        nwords.append(stnew)
     else:
        nwords.append(word[::-1])
