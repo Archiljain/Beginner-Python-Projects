0e06bc03be471571caf42ae92ba6380c1f43d0de
Archil <jainarchil55@gmail.com>
2025-07-10 18:01:26 +0530

    added a new game

--- exponential code/main.py
+++
@@ -1,5 +0,0 @@
-# WAP in python to calculate the poower of any number
-base=int(input("enter the base"))
-exponent=int(input("enter the exponent"))
-power=base**exponent
-print("base to the power exponent is",power)
